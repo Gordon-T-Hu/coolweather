df9c211edf55a7546bfc7802471341353756227e
Hu Tao <hutao@huaqin.com>
2016-06-01 19:24:55 +0800

    modify MainActivity.java path

--- app/src/main/java/com/hutao/coolweather/activity/MainActivity.java
+++ app/src/main/java/com/hutao/coolweather/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.hutao.coolweather;
+package com.hutao.coolweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.hutao.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
