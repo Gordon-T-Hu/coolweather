96c60e0e46f7e8e6d022a0f2c8c3947e009676fd
Hu Tao <hutao@huaqin.com>
2016-06-01 19:39:57 +0800

    add province,city and country

---
+++ app/src/main/java/com/hutao/coolweather/model/Province.java
@@ -0,0 +1,35 @@
+package com.hutao.coolweather.model;
+
+/**
+ * Created by ubuntu on 16-6-1.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+}
